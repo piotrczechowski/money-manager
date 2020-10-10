e1def3a2888b58cd78151ae8ea6f0ce539ef95d2
maniek <mpastuszka@gmail.com>
2020-10-10 10:21:57 +0200

    [init] - initial commit

---
+++ src/test/java/pl/sda/moneymanager/MoneyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.moneymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
