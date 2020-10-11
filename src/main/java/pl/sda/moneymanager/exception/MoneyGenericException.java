1ead193b24c9aa4544ae8133c91a8db1d779df6e
maniek <mpastuszka@gmail.com>
2020-10-11 15:36:47 +0200

    [change] - exception handling continues..

---
+++ src/main/java/pl/sda/moneymanager/exception/MoneyGenericException.java
@@ -0,0 +1,19 @@
+package pl.sda.moneymanager.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class MoneyGenericException extends RuntimeException {
+
+    public MoneyGenericException() {
+    }
+
+    public MoneyGenericException(String message) {
+        super(message);
+    }
+
+    public MoneyGenericException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
