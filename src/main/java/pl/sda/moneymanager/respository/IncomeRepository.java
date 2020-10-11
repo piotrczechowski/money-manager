068b70dd1e7a2430d63264db25251c92cd7b8488
PiotrCzechowski <czechowp@gmail.com>
2020-10-11 13:36:45 +0200

    [fix] - entity mapping fixes 2

--- src/main/java/pl/sda/moneymanager/respository/IncomeRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.sda.moneymanager.respository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.sda.moneymanager.domain.Income;
-
-public interface IncomeRepository extends JpaRepository<Income, Long> {
-}
