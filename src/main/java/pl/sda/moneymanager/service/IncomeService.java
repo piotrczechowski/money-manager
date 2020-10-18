417dd09daf336e5af5f1f49ec261bb681792ec0a
maniek <mpastuszka@gmail.com>
2020-10-18 15:27:06 +0200

    [refactor] - changing entities into dtos in income controller

--- src/main/java/pl/sda/moneymanager/service/IncomeService.java
+++ src/main/java/pl/sda/moneymanager/service/IncomeService.java
@@ -3,48 +3,60 @@ package pl.sda.moneymanager.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import pl.sda.moneymanager.converter.IncomeConverter;
 import pl.sda.moneymanager.domain.Income;
+import pl.sda.moneymanager.dto.IncomeDto;
 import pl.sda.moneymanager.repository.IncomeRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class IncomeService {
 
     private final IncomeRepository incomeRepository;
+    private final IncomeConverter incomeConverter;
 
-    public IncomeService(final IncomeRepository incomeRepository) {
+    public IncomeService(final IncomeRepository incomeRepository, final IncomeConverter incomeConverter) {
         this.incomeRepository = incomeRepository;
+        this.incomeConverter = incomeConverter;
     }
 
-    public List<Income> readIncomesWithQueryParams(int pageNumber, int pageSize) {
+    public List<IncomeDto> readIncomesWithQueryParams(int pageNumber, int pageSize) {
         log.info("reading incomes with query params, page number = [{}], page size = [{}]", pageNumber,
             pageSize);
 
         var result = incomeRepository.findAll(PageRequest.of(pageNumber, pageSize)).getContent();
 
         log.info("result from db: {}", result);
-        return result;
+
+        return convertIncomes(result);
     }
 
-    public List<Income> readAllIncomes() {
+    private List<IncomeDto> convertIncomes(List<Income> result) {
+        return result.stream()
+            .map(incomeConverter::fromEntityToDto)
+            .collect(Collectors.toList());
+    }
+
+    public List<IncomeDto> readAllIncomes() {
         log.info("reading all incomes");
 
         var result = incomeRepository.findAll();
 
         log.info("number of read elements: [{}]", result.size());
         log.debug("result : {}", result);
-        return result;
+        return convertIncomes(result);
     }
 
-    public Optional<Income> findIncomeById(Long id) {
+    public Optional<IncomeDto> findIncomeById(Long id) {
         log.info("find income by id: [{}]", id);
 
         var result = incomeRepository.findById(id);
         log.info("result: [{}]", result);
-        return result;
+        return result.map(incomeConverter::fromEntityToDto);
     }
 
     public boolean deleteIncomeById(Long id) {
