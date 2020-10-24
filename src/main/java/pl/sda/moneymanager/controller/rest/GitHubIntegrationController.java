43c73bce44884c28cf16735ec77fb7e5ee9ef302
maniek <mpastuszka@gmail.com>
2020-10-24 12:36:01 +0200

    [refactor] - code improvements for github integration

--- src/main/java/pl/sda/moneymanager/controller/rest/GitHubIntegrationController.java
+++ src/main/java/pl/sda/moneymanager/controller/rest/GitHubIntegrationController.java
@@ -3,32 +3,24 @@ package pl.sda.moneymanager.controller.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import pl.sda.moneymanager.dto.GithubRepoDto;
+import pl.sda.moneymanager.service.GithubService;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/github-int")
 public class GitHubIntegrationController {
 
-    private static final String myReposUrl = "https://api.github.com/users/mariuszpastuszka/repos";
-    private final RestTemplate restTemplate;
+    private final GithubService githubService;
 
-    public GitHubIntegrationController(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public GitHubIntegrationController(final GithubService githubService) {
+        this.githubService = githubService;
     }
 
     @GetMapping("/my-repos")
     public List<GithubRepoDto> myRepos() {
 
-//        String[].class
-//        List.class
-//        List<String>.class
-//        List<GithubRepoDto>.class
-        var requestResult = restTemplate.getForObject(myReposUrl, GithubRepoDto[].class);
-        return Arrays.asList(requestResult);
+        return githubService.allUserRepos();
     }
 }
---
+++ src/main/java/pl/sda/moneymanager/service/GithubService.java
@@ -0,0 +1,32 @@
+package pl.sda.moneymanager.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import pl.sda.moneymanager.dto.GithubRepoDto;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@Slf4j
+public class GithubService {
+    private static final String myReposUrl = "https://api.github.com/users/mariuszpastuszka/repos";
+    private final RestTemplate restTemplate;
+
+    public GithubService(final RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public List<GithubRepoDto> allUserRepos() {
+//        String[].class
+//        List.class
+//        List<String>.class
+//        List<GithubRepoDto>.class
+        var requestResult = restTemplate.getForObject(myReposUrl, GithubRepoDto[].class);
+
+//        return Arrays.asList(requestResult != null ? requestResult : new GithubRepoDto[]{});
+        return requestResult != null ? Arrays.asList(requestResult) : Collections.emptyList();
+    }
+}
