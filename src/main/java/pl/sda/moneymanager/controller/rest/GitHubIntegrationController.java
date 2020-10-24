0e2bc6ffb5681e340bc9d2823584ec2db806936d
maniek <mpastuszka@gmail.com>
2020-10-24 15:48:29 +0200

    [add] - endpoint for creation of github repo objects

--- src/main/java/pl/sda/moneymanager/controller/rest/GitHubIntegrationController.java
+++ src/main/java/pl/sda/moneymanager/controller/rest/GitHubIntegrationController.java
@@ -1,13 +1,17 @@
 package pl.sda.moneymanager.controller.rest;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.sda.moneymanager.dto.GithubRepoDto;
 import pl.sda.moneymanager.service.GithubService;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -31,5 +35,12 @@ public class GitHubIntegrationController {
     public List<GithubRepoDto> userRepos(@PathVariable("userName") String userName) {
         log.info("user repos, user name = [{}]", userName);
         return githubService.allReposOfGivenUser(userName);
+    }
+
+    // Create = 201 + header location
+    @PostMapping("/repos")
+    public ResponseEntity<GithubRepoDto> createRepo(@RequestBody GithubRepoDto repoToCreate) {
+        log.info("github repo to create: [{}]", repoToCreate);
+        return ResponseEntity.created(URI.create("/repos/1")).build();
     }
 }
