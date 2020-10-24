6449a8ec51a2e967650b349a46ddd7374499236b
maniek <mpastuszka@gmail.com>
2020-10-24 13:49:39 +0200

    [add] - new github endpoint with configurable parameter

--- src/main/java/pl/sda/moneymanager/controller/rest/GitHubIntegrationController.java
+++ src/main/java/pl/sda/moneymanager/controller/rest/GitHubIntegrationController.java
@@ -1,6 +1,8 @@
 package pl.sda.moneymanager.controller.rest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.sda.moneymanager.dto.GithubRepoDto;
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/github-int")
+@Slf4j
 public class GitHubIntegrationController {
 
     private final GithubService githubService;
@@ -20,7 +23,13 @@ public class GitHubIntegrationController {
 
     @GetMapping("/my-repos")
     public List<GithubRepoDto> myRepos() {
-
+        log.info("my repos");
         return githubService.allUserRepos();
+    }
+
+    @GetMapping("/repos/{userName}")
+    public List<GithubRepoDto> userRepos(@PathVariable("userName") String userName) {
+        log.info("user repos, user name = [{}]", userName);
+        return githubService.allReposOfGivenUser(userName);
     }
 }
--- src/main/java/pl/sda/moneymanager/service/GithubService.java
+++ src/main/java/pl/sda/moneymanager/service/GithubService.java
@@ -8,11 +8,14 @@ import pl.sda.moneymanager.dto.GithubRepoDto;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @Slf4j
 public class GithubService {
-    private static final String myReposUrl = "https://api.github.com/users/mariuszpastuszka/repos";
+    public static final String REPO_USER = "user";
+    private static final String myReposUrl = String.format("https://api.github.com/users/{%s}/repos",
+        REPO_USER);
     private final RestTemplate restTemplate;
 
     public GithubService(final RestTemplate restTemplate) {
@@ -20,13 +23,25 @@ public class GithubService {
     }
 
     public List<GithubRepoDto> allUserRepos() {
+
+        return allReposOfGivenUser("mariuszpastuszka");
+    }
+
+    public List<GithubRepoDto> allReposOfGivenUser(String repoUser) {
+        log.info("getting all repos for user: [{}]", repoUser);
+
+        Map<String, ?> params = Map.of(REPO_USER, repoUser);
 //        String[].class
 //        List.class
 //        List<String>.class
 //        List<GithubRepoDto>.class
-        var requestResult = restTemplate.getForObject(myReposUrl, GithubRepoDto[].class);
+        var requestResult = restTemplate.getForObject(myReposUrl, GithubRepoDto[].class,
+            params);
 
+        log.info("number of elements in result: [{}]", requestResult != null ? requestResult.length : 0);
+        log.debug("all repos: {}", Arrays.toString(requestResult));
 //        return Arrays.asList(requestResult != null ? requestResult : new GithubRepoDto[]{});
         return requestResult != null ? Arrays.asList(requestResult) : Collections.emptyList();
+
     }
 }
